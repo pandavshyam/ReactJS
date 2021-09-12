8386455c07ec9cb24d50c4f370099bc7549ad433
shyampandav <shyam.pandav@sell.do>
2021-09-12 12:03:40 +0530

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,40 +1,34 @@
 import React, { Component } from "react";
 import Menu from "./MenuComponent";
 import { DISHES } from "../shared/dishes";
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from "./DishdetailComponent";
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES,
-      selectedDish: null,
+      dishes: DISHES
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({
-      selectedDish: dishId,
-    });
-  }
-
   render() {
+    const HomePage = () => {
+      return(
+          <Home />
+      );
+    }
     return (
       <div>
         <Header/>
-        <Menu
-          dishes={this.state.dishes}
-          onClick={(dishId) => this.onDishSelect(dishId)}
-        />
-        <DishDetail
-          selectedDish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
+        <Switch>
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer/>
       </div>
     );
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
 function RendorMenuItem({ dish, onClick }) {
   return (
-    <Card key={dish.id} onClick={() => onClick(dish.id)}>
+    <Card key={dish.id}>
       <CardImg width="100%" src={dish.image} alt={dish.name} />
       <CardImgOverlay>
         <CardTitle> {dish.name} </CardTitle>
